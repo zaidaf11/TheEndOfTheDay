3b4cdcc31771ce85551e4d0376e319b99661ffc8
zaidaf11 <zangirrhehe@gmail.com>
2026-04-08 10:05:20 +0700

    p

--- TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
+++ TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
@@ -13,5 +13,6 @@ public class TheEndOfTheDay {
     public static void main(String[] args) {
         System.out.println("The End Of The Day: The Origin 1");
         System.out.println("The End Of The Day: The Origin 2");
+        System.out.println("The End Of The Day: The Origin 3");
     }
 }
