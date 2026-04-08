150762300e2b78d7662d80c9fb528fe3edd3671c
Zaid Akmal Firdaus <zangirrhehe@gmail.com>
2026-04-08 10:28:19 +0700

    Add files via upload

---
+++ TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.theendoftheday;
+
+/**
+ *
+ * @author Zaid Akmal Firdaus
+ */
+public class TheEndOfTheDay {
+
+    public static void main(String[] args) {
+        System.out.println("The End Of The Day: The Origin 1");
+        System.out.println("The End Of The Day: New Journey 2");
+        System.out.println("The End Of The Day: Big Bang 3");
+        System.out.println("The End Of The Day: Last Soul 4");
+    }
+}
