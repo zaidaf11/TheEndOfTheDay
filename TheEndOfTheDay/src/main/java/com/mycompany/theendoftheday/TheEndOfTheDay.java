6ba42dce9cab628f798827fa1a8d99c9b112578c
zaidaf11 <zangirrhehe@gmail.com>
2026-04-08 10:48:27 +0700

    AWAS JOMBI KOPO!

--- TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
+++ TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
@@ -15,6 +15,8 @@ public class TheEndOfTheDay {
         System.out.println("The End Of The Day: New Journey 2");
         System.out.println("The End Of The Day: Big Bang 3");
         System.out.println("The End Of The Day: Last Soul 4");
+        System.out.println("The End Of The Day: DLC AWAS ZOMBIE KOPO");
         //Fatan Anjing
+        //AWAS ORANG KOPO!
     }
 }
