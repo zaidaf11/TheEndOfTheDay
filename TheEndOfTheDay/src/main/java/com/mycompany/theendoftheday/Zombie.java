6e4e42e1949a98b662a979ac7f4923208410d1a9
Zaid Akmal Firdaus <zangirrhehe@gmail.com>
2026-04-08 09:56:50 +0700

    Add files via upload

---
+++ TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/Zombie.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.theendoftheday;
+
+/**
+ *
+ * @author Zaid Akmal
+ */
+public class Zombie {
+    protected int HP;
+    protected int Damage;
+    
+    public void attack (Player P)
+    {P.setHP(P.getHP()-Damage);
+    System.out.println("Zombie menyerang!!"+Damage+HP);
+    }
+    
+    public String getType(){
+        return "Zombie";
+    }
+    
+}
