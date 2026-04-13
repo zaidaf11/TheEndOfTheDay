d0a7ac7efccf61c558f21254405dd018c9b9a3df
zaidaf11 <zangirrhehe@gmail.com>
2026-04-13 11:59:51 +0700

    update terbaru
    +main program
    +action menu
    +status

--- TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
+++ TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
@@ -8,9 +8,41 @@ package com.mycompany.theendoftheday;
  *
  * @author Zaid Akmal Firdaus
  */
+import java.util.Scanner;
+import java.util.Random;
 public class TheEndOfTheDay {
-
+    static Scanner input = new Scanner(System.in);
+    static Random rand = new Random();
+    static Player player = new Player();
+    static int turn = 1;
+    
     public static void main(String[] args) {
-        System.out.println("The End Of The Day: The Origin");
+        Intro();
+        gameloop();
+    }
+        public static void Intro() {
+        System.out.println("Kamu terbangun di tengah-tengah kerumunan para Zombie!");
+        System.out.println("Hari mulai mencekam, apa yang akan kamu lakukan?");
+        }
+        static void gameloop() {
+            System.out.println("\n=== TURN " + turn + " ===");
+            showStatus();
+        
+    }
+
+    static void showStatus() {
+        System.out.println("HP; " + player.getHP());
+        System.out.println("Stamina; " + player.getStamina());
+        System.out.println("Hunger; " + player.getHunger());
+        System.out.println("Weapon; " + player.getWeapon());
+        
+        
+    }
+    static void actionMenu () {
+        System.out.println("1. Cari Senjata");
+        System.out.println("2. Serang Zombie");
+        System.out.println("3. Sembunyi");
+        System.out.println("4. Lari");
+        System.out.println("5. Istirahat");
     }
 }
