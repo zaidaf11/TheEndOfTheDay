b74b7ecff17348b3ac2786df978e3ace41aef10e
zaidaf11 <zangirrhehe@gmail.com>
2026-04-13 07:50:59 +0700

    p

--- TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
+++ TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
@@ -11,12 +11,6 @@ package com.mycompany.theendoftheday;
 public class TheEndOfTheDay {
 
     public static void main(String[] args) {
-        System.out.println("The End Of The Day: The Origin 1");
-        System.out.println("The End Of The Day: New Journey 2");
-        System.out.println("The End Of The Day: Big Bang 3");
-        System.out.println("The End Of The Day: Last Soul 4");
-        System.out.println("The End Of The Day: DLC AWAS ZOMBIE KOPO");
-        //Fatan Anjing
-        //AWAS ORANG KOPO!
+        System.out.println("The End Of The Day: The Origin");
     }
 }
