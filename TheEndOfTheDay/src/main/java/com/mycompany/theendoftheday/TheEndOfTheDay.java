202f1ac2a01641f8e0673d2a6b81b6aa84836587
Rasya Adrian <rasyaadrianalfarisy@gmail.com>
2026-04-13 12:02:11 +0700

    l

--- TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
+++ TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/TheEndOfTheDay.java
@@ -8,15 +8,10 @@ package com.mycompany.theendoftheday;
  *
  * @author Zaid Akmal Firdaus
  */
-public class TheEndOfTheDay {
+import java.util.Scanner;
+import java.util.Random;
 
-    public static void main(String[] args) {
-        System.out.println("The End Of The Day: The Origin 1");
-        System.out.println("The End Of The Day: New Journey 2");
-        System.out.println("The End Of The Day: Big Bang 3");
-        System.out.println("The End Of The Day: Last Soul 4");
-        System.out.println("The End Of The Day: DLC AWAS ZOMBIE KOPO");
-        //Fatan Anjing
-        //AWAS ORANG KOPO!
+public class TheEndOfTheDay {
+    static Scanner input = new scanner();
     }
-}
+
