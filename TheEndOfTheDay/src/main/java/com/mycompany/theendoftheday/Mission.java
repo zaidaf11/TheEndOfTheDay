f4b8b1f3cc779dedcc28d11b5ec6a9a992f9699f
zaidaf11 <zangirrhehe@gmail.com>
2026-04-27 11:43:58 +0700

    pe

---
+++ TheEndOfTheDay/src/main/java/com/mycompany/theendoftheday/Mission.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.theendoftheday;
+
+/**
+ *
+ * @author Zaid Akmal
+ */
+public class Mission {
+    
+    private Location [] locations;
+    
+    public Mission(){
+        locations = new Location[]{
+            new Location(
+             "Sekolah")
+                
+            new Location(
+            "Rumah sakit")
+        };
+        
+    }
+
+}
